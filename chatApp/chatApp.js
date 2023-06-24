d1e52c3c04a817934fbb80077333e8762f9fb5b6
PriyankaThakur21 <thakur.pri112@gmail.com>
2023-06-24 18:11:16 +0530

    getallmsg

--- chatApp/chatApp.js
+++ chatApp/chatApp.js
@@ -1,12 +1,20 @@
 window.addEventListener('DOMContentLoaded', async ()=>{
-    try{
     const token = localStorage.getItem('token');
+    try{
     const users= await axios.get('http://localhost:3000/getAllusers', {headers: {"Authorization": token}});
     console.log(users.data)
     showonscreen(users.data);
 }
     catch(error){
         console.log(error);
+    }
+    try{
+        const msg = await axios.get('http://localhost:3000/getmsg', {headers: {"Authorization": token}});
+        console.log(msg.data);
+        showmsgonscreen(msg.data);
+    }
+    catch(err){
+        console.log(err);
     }
 })
 
@@ -15,6 +23,18 @@ const a = document.getElementById('block');
 a.innerHTML = `<div><p>You joined</p></div>`;
 users.forEach(user => {
     a.innerHTML += `<div><p>${user.name} joined</p></div>`;
+});
+}
+
+function showmsgonscreen(msg){
+    const msgBox = document.getElementById('block2');
+    msg.chat.forEach(m => {
+        if(m.userId === msg.id){
+            msgBox.innerHTML += `<div><h5>${m.message}</h5></div>`;
+        }
+        else{
+            msgBox.innerHTML += `<div style="display:flex; justify-content: flex-end;"><h5>${m.message}</h5></div>`;
+        }
 });
 }
 
